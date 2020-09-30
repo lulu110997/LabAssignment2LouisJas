da579883c84008a051fd5a6c42b05b779e40fda1
lulu110997 <53894562+lulu110997@users.noreply.github.com>
2020-10-01 02:31:46 +1000

    UR10e DH param and advanced GUI using RMRC

---
+++ createUR10eModel.m
@@ -0,0 +1,62 @@
+function ur10e = createUR10eModel(workspace, scale)
+if nargin < 2
+    scale = 0.5;
+end
+if nargin < 1
+    workspace = [-3 3 -2.5 2 -0.25 2];
+    scale = 0.5;
+end
+
+% DH parameters
+L1 = Link('d', 0.1807, 'a', 0, 'alpha', pi/2, 'qlim', [-2*pi, 2*pi]);
+L2 = Link('d', 0,'a', -0.6127, 'alpha', 0, 'qlim', [deg2rad(-360), deg2rad(360)]);
+L3 = Link('d', 0, 'a', -0.51755, 'alpha', 0, 'qlim', [deg2rad(-360), deg2rad(360)]);
+L4 = Link('d', 0.17415, 'a', 0, 'alpha', pi/2, 'qlim', [-2*pi, 2*pi]);
+L5 = Link('d', 0.11985, 'a', 0, 'alpha', -pi/2, 'qlim', [-2*pi, 2*pi]);
+L6 = Link('d', 0.11655, 'a', 0, 'alpha', 0, 'qlim', [-2*pi, 2*pi]);
+
+% Fix robot base rotation to a specific orientation
+
+ur10eModel = SerialLink([L1 L2 L3 L4 L5 L6],'name','ur10e');
+
+% for linkIndex = 0:ur10eModel.n
+%     [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['ur10eLink',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+%     ur10eModel.faces{linkIndex+1} = faceData;
+%     ur10eModel.points{linkIndex+1} = vertexData;
+% end
+% 
+% % Display robot
+% ur10eModel.plotopt3d = {'wrist', 'xyz', 'arrow'};
+% ur10eModel.plot3d(zeros(1,6), 'workspace', workspace, 'scale', scale)
+% if isempty(findobj(get(gca,'Children'),'Type','Light'))
+%     camlight
+% end
+% ur10eModel.delay = 0;
+% 
+% % Try to correctly colour the arm (if colours are in ply file data)
+% for linkIndex = 0:ur10eModel.n
+%     handles = findobj('Tag', ur10eModel.name);
+%     h = get(handles,'UserData');
+%     try
+%         h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+%             , plyData{linkIndex+1}.vertex.green ...
+%             , plyData{linkIndex+1}.vertex.blue]/255;
+%         h.link(linkIndex+1).Children.FaceColor = 'interp';
+%     catch ME_1
+%         disp(ME_1);
+%         continue;
+%     end
+% end
+
+    ur10eModel.plotopt = {'notiles', 'nobase', 'noshadow'};
+    ur10eModel.base = ur10eModel.base*transl(0,0,0.5);
+    ur10eModel.plot([0 -pi/2 pi/2 -pi/2 -pi/2 0],'workspace',workspace, 'scale', scale);
+    
+    ur10e = ur10eModel();
+    hold on
+    %%%%% testing %%%%%%%%%%%%%%%%%%%
+    PlaceObject('pallet.ply', [-0.6374, 0, 0]);
+    PlaceObject('safetyfence.ply', [ 0 0 -0.138]);
+%     ur10eModel.teach;
+%     view([0, 0])
+end
